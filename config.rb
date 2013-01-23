23e1ed8e9ae4e064967993c60a259eb132e53fd6
Claudio Sanches <contato@claudiosmweb.com>
2013-01-23 00:53:39 -0200

    added environment to compass

--- config.rb
+++ config.rb
@@ -9,7 +9,7 @@ javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
-output_style = :expanded
+output_style = (environment == :production) ? :compressed : :expanded
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 relative_assets = true
