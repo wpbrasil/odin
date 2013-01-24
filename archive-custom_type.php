dc0929376b86cfbd0e571fb9d9cdc6a079078bad
Claudio Sanches <contato@claudiosmweb.com>
2013-01-24 00:07:50 -0200

    added sample files to CPTs

---
+++ archive-custom_type.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Sample archive for Custom Post Type
+ */
+get_header(); ?>
+<div id="primary">
+    <section id="content" role="main">
+        <?php if ( have_posts() ) : ?>
+            <header class="page-header">
+                <h1 class="page-title"><?php post_type_archive_title(); ?></h1>
+            </header>
+            <?php while ( have_posts() ) : the_post(); ?>
+                <?php get_template_part( 'loop' ); ?>
+            <?php endwhile; ?>
+            <?php echo odin_pagination(); ?>
+        <?php else : ?>
+            <?php get_template_part( 'no-results' ); ?>
+        <?php endif; ?>
+    </section><!-- #content -->
+</div><!-- #primary -->
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
