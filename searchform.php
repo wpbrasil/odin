fe2f21e36c63ce4024ac9d253a3f2787155e4904
Claudio Sanches <contato@claudiosmweb.com>
2013-01-23 23:42:52 -0200

    added serachform template

---
+++ searchform.php
@@ -0,0 +1,4 @@
+<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+    <input type="text" class="input-text" name="s" id="s" />
+    <input type="submit" class="input-submit" value="<?php esc_attr_e( 'Buscar', '_base' ); ?>" />
+</form>
