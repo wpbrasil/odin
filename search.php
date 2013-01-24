b04b5f044574a9e0cb2819474050f129927706aa
Claudio Sanches <contato@claudiosmweb.com>
2013-01-23 23:42:06 -0200

    added search template

---
+++ search.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+<div id="primary">
+    <section id="content" role="main">
+        <?php if ( have_posts() ) : ?>
+            <header class="page-header">
+                <h1 class="page-title"><?php echo __( 'Resultados da Pesquisa para: ', 'odin' ) . '<span>' . get_search_query() . '</span>'; ?></h1>
+            </header>
+            <?php while ( have_posts() ) : the_post(); ?>
+                <?php get_template_part( 'loop' ); ?>
+            <?php endwhile; ?>
+            <?php echo odin_pagination(); ?>
+        <?php else : ?>
+            <?php get_template_part( 'no-results' ); ?>
+        <?php endif; ?>
+    </section><!-- #content -->
+</div><!-- #primary -->
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
