07c33e8a644a2b508901d61a2ce8444f0f95ce3b
Claudio Sanches <contato@claudiosmweb.com>
2013-01-27 13:19:29 -0200

    fixed the footer

--- footer.php
+++ footer.php
@@ -1,8 +1,9 @@
     </div><!-- #main -->
     <footer id="footer" role="contentinfo">
-        <span>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a> - <?php _e( 'Todos os Direitos Reservados', 'odin' ); ?> | Powered by the <a href="http://wpbrasil.github.com/odin" rel="nofollow" target="_blank">Odin</a> forces and <a href="http://wordpress.org/" rel="nofollow" target="_blank">WordPress</a>.</span>
+        <span>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a> - <?php _e( 'Todos os Direitos Reservados', 'odin' ); ?> | <?php echo sprintf( __( 'Powered by the <a href="%s" rel="nofollow" target="_blank">Odin</a> forces and <a href="%s" rel="nofollow" target="_blank">WordPress</a>.', 'odin' ), 'http://wpbrasil.github.com/odin', 'http://wordpress.org/' ); ?></span>
     </footer><!-- #footer -->
 </div><!-- .wrapper -->
 <?php wp_footer(); ?>
 </body>
 </html>
+
