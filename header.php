b3e4717aecdc180ef5933efbc3a42db9552f3f0e
Claudio Sanches <contato@claudiosmweb.com>
2013-01-27 01:38:41 -0200

    removed js/vendor folder

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/html5-3.6-respond-1.1.0.min.js" type="text/javascript"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5-3.6-respond-1.1.0.min.js" type="text/javascript"></script>
 <![endif]-->
 <?php wp_head(); ?>
 </head>
