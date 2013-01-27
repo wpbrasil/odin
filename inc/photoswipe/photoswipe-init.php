de156687cc3f6a45f44eedd75bf181e936b1a4d1
Deblyn <deblynprado@hotmail.com>
2013-01-27 17:33:32 -0200

    Change the call in photoswipe scripts

--- inc/photoswipe/photoswipe-init.php
+++ inc/photoswipe/photoswipe-init.php
@@ -1,18 +1,13 @@
 <?php
 /**
- * PhotoSwipe functions.
+ * Load PhotoSwipe functions.
  */
 
-/**
- * Load photoswipe script.
- */
 function odin_photoswipe_scripts() {
-    wp_enqueue_script( 'jquery');
-    wp_register_script( 'klass', get_template_directory_uri() . '/inc/photoswipe/js/klass.min.js', null, null, true );
+	wp_register_script( 'klass', get_template_directory_uri() . '/inc/photoswipe/js/klass.min.js', array( 'jquery' ), null, true );
     wp_enqueue_script( 'klass' );
 	
-	wp_enqueue_script( 'jquery');
-    wp_register_script( 'photoswipe', get_template_directory_uri() . '/inc/photoswipe/js/code.photoswipe-3.0.5.min.js', null, null, true );
+	wp_register_script( 'photoswipe', get_template_directory_uri() . '/inc/photoswipe/js/code.photoswipe-3.0.5.min.js', array( 'jquery' ), null, true );
     wp_enqueue_script( 'photoswipe' );
 	
 	wp_register_style( 'photoswipe', get_template_directory_uri() . '/inc/photoswipe/css/photoswipe.css', array(), null, 'all' );
