52f712890f7d2283152c96f50b20e8f95e13b28e
Claudio Sanches <contato@claudiosmweb.com>
2013-01-27 02:53:47 -0200

    added "Powered by..." in footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
     </div><!-- #main -->
     <footer id="footer" role="contentinfo">
-        <span>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a> - <?php _e( 'Todos os Direitos Reservados', 'odin' ); ?></span>
+        <span>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a> - <?php _e( 'Todos os Direitos Reservados', 'odin' ); ?> | Powered by the <a href="http://wpbrasil.github.com/odin" rel="nofollow" target="_blank">Odin</a> forces and <a href="http://wordpress.org/" rel="nofollow" target="_blank">WordPress</a>.</span>
     </footer><!-- #footer -->
 </div><!-- .wrapper -->
 <?php wp_footer(); ?>
