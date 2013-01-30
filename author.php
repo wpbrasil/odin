7b39e8c595c859ba29f2325dce131429b90c6834
Ruan Barbosa <ruancwb@gmail.com>
2013-01-30 00:28:58 -0200

    added schema.org markup

--- author.php
+++ author.php
@@ -1,16 +1,16 @@
 <?php get_header(); ?>
 <div id="primary">
-    <section id="content" role="main">
+    <section id="content" role="main" itemscope="" itemtype="http://schema.org/Person">
         <?php if ( have_posts() ) : the_post(); ?>
             <header class="page-header">
-                <h1 class="page-title author"><?php echo __( 'Arquivos do Autor: ', 'odin' ) . '<span class="vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>'; ?></h1>
+                <h1 class="page-title author" itemprop="name"><?php echo __( 'Arquivos do Autor: ', 'odin' ) . '<span class="vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me" itemprop="url">' . get_the_author() . '</a></span>'; ?></h1>
             </header>
             <?php rewind_posts(); ?>
             <?php if ( get_the_author_meta( 'description' ) ) : ?>
                 <div class="author-info">
                     <h2><?php echo __( 'Sobre ', 'odin' ) . get_the_author(); ?></h2>
-                    <span class="author-avatar"><?php echo get_avatar( get_the_author_meta( 'ID' ), 60 ); ?></span>
-                    <span class="author-description"><?php the_author_meta( 'description' ); ?></span>
+                    <span class="author-avatar" itemprop="image"><?php echo get_avatar( get_the_author_meta( 'ID' ), 60 ); ?></span>
+                    <span class="author-description" itemprop="description"><?php the_author_meta( 'description' ); ?></span>
                 </div><!-- .author-info -->
             <?php endif; ?>
             <?php while ( have_posts() ) : the_post(); ?>
