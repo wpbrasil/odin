31827150c90ef5bbd8c0e6def168ab4df697c7a8
Ruan Barbosa <ruancwb@gmail.com>
2013-01-30 00:29:38 -0200

    added schema.org markup

--- category.php
+++ category.php
@@ -3,11 +3,11 @@
     <section id="content" role="main">
         <?php if ( have_posts() ) : ?>
             <header class="page-header">
-                <h1 class="page-title"><?php echo __( 'Arquivos da Categoria: ', 'odin' ) . '<span>' . single_cat_title( '', false ) . '</span>'; ?></h1>
+                <h1 class="page-title" itemprop="name headline"><?php echo __( 'Arquivos da Categoria: ', 'odin' ) . '<span>' . single_cat_title( '', false ) . '</span>'; ?></h1>
                 <?php
                     $category_description = category_description();
                     if ( ! empty( $category_description ) ) {
-                        echo '<div class="category-archive-meta">' . $category_description . '</div>';
+                        echo '<div class="category-archive-meta" itemprop="description">' . $category_description . '</div>';
                     }
                 ?>
             </header>
