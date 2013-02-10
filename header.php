d7b1aa58de18f97f754d125e6d9475c25a440b0c
Claudio Sanches <contato@claudiosmweb.com>
2013-02-10 03:57:27 -0200

    updated html5.min.js

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <link href="<?php echo get_template_directory_uri(); ?>/images/favicon.ico" rel="shortcut icon" />
 <!--[if lt IE 9]>
-<script src="<?php echo get_template_directory_uri(); ?>/js/html5-3.6-respond-1.1.0.min.js" type="text/javascript"></script>
+<script src="<?php echo get_template_directory_uri(); ?>/js/html5.min.js" type="text/javascript"></script>
 <![endif]-->
 <?php wp_head(); ?>
 </head>
