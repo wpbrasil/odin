13652f9df13790ef6850e91d14b5d66427688aab
claudiosmweb <contato@claudiosmweb.com>
2013-03-24 16:19:42 -0300

    moved compass file to build folder #42

--- build/config.rb
+++ build/config.rb
@@ -2,10 +2,10 @@
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "./"
-sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "js"
+css_dir = "../"
+sass_dir = "../sass"
+images_dir = "../images"
+javascripts_dir = "../js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
