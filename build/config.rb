5f253e8a968fde6222cb0d162d2965b68ac028cb
claudiosmweb <contato@claudiosmweb.com>
2013-03-24 16:20:58 -0300

    fixed compass config file #42

--- build/config.rb
+++ build/config.rb
@@ -1,8 +1,8 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "../"
+http_path = ""
+css_dir = ".."
 sass_dir = "../sass"
 images_dir = "../images"
 javascripts_dir = "../js"
