7dbc175aac71642c6246eff54896ac37735bb2cd
claudiosmweb <contato@claudiosmweb.com>
2013-03-24 02:51:09 -0300

    fixed sidebar marking

--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,7 @@
-<aside>
-	<div id="secondary" class="widget-area" role="complementary" itemscope="" itemtype="http://schema.org/WPSideBar">
-	    <?php if ( ! dynamic_sidebar( 'main-sidebar' ) ) : ?>
-	        <?php the_widget( 'WP_Widget_Recent_Posts', array( 'number' => 10 ) ); ?>
-	        <?php the_widget( 'WP_Widget_Archives', array( 'count' => 0, 'dropdown' => 1 ) ); ?>
-	        <?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
-	    <?php endif; ?>
-	</div><!-- #secondary -->
-</aside>
+<div id="secondary" class="widget-area" role="complementary" itemscope="" itemtype="http://schema.org/WPSideBar">
+    <?php if ( ! dynamic_sidebar( 'main-sidebar' ) ) : ?>
+        <?php the_widget( 'WP_Widget_Recent_Posts', array( 'number' => 10 ) ); ?>
+        <?php the_widget( 'WP_Widget_Archives', array( 'count' => 0, 'dropdown' => 1 ) ); ?>
+        <?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+    <?php endif; ?>
+</div><!-- #secondary -->
