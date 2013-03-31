8058278fee2269eb62a115973f07678fbab4d553
Rafael Funchal <rafaelfunchal@gmail.com>
2013-03-31 07:43:46 -0300

    Fixing sidebar id

--- functions.php
+++ functions.php
@@ -102,7 +102,7 @@ function odin_widgets_init() {
     register_sidebar(
         array(
             'name' => __( 'Sidebar Principal', 'odin' ),
-            'id' => 'mao=in-sidebar',
+            'id' => 'main-sidebar',
             'description' => __( 'Sidebar Principal do site', 'odin' ),
             'before_widget' => '<aside id="%1$s" class="widget %2$s">',
             'after_widget' => '</aside>',
