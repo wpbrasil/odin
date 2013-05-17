789cc1f6727e2bcbd2a37d63b7a03fe537296acb
claudiosmweb <contato@claudiosmweb.com>
2013-05-17 01:50:12 -0300

    fixed searchform.php in favor to bootstrap

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
 <form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
     <label for="s" class="assistive-text"><?php _e( 'Busca', 'odin' ); ?></label>
-    <input type="text" class="input-text" name="s" id="s" />
-    <input type="submit" class="input-submit" value="<?php esc_attr_e( 'Buscar', 'odin' ); ?>" />
+    <input type="text" name="s" id="s" />
+    <input type="submit" class="btn btn-primary" value="<?php esc_attr_e( 'Buscar', 'odin' ); ?>" />
 </form>
