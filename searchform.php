1352a18c5783812d3437c859b573de1109c4bdce
claudiosmweb <contato@claudiosmweb.com>
2013-05-17 01:53:01 -0300

    refixed searchform.php in favor to bootstrap

--- searchform.php
+++ searchform.php
@@ -1,5 +1,5 @@
-<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+<form method="get" id="searchform" class="form-search" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
     <label for="s" class="assistive-text"><?php _e( 'Busca', 'odin' ); ?></label>
-    <input type="text" name="s" id="s" />
-    <input type="submit" class="btn btn-primary" value="<?php esc_attr_e( 'Buscar', 'odin' ); ?>" />
+    <input type="text" class="input-medium search-query" name="s" id="s" />
+    <input type="submit" class="btn" value="<?php esc_attr_e( 'Buscar', 'odin' ); ?>" />
 </form>
