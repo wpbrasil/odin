673339d8e42ba710f4bf6a64dc1880a07a6b7758
claudiosmweb <contato@claudiosmweb.com>
2013-05-17 03:19:33 -0300

    fixed the 404 page

--- 404.php
+++ 404.php
@@ -8,15 +8,21 @@
             <div class="entry-content">
                 <p><?php _e( 'Parece que n&atilde;o encontramos o que voc&ecirc; est&aacute; procurando. Talvez a busca, ou um dos links abaixo, possa ajudar.', 'odin' ); ?></p>
                 <?php get_search_form(); ?>
-                <?php the_widget( 'WP_Widget_Recent_Posts', array( 'number' => 10 ), array( 'widget_id' => '404' ) ); ?>
-                <div class="widget">
+                <div class="span3 no-margin-left">
+                    <?php the_widget( 'WP_Widget_Recent_Posts', array( 'number' => 10 ), array( 'widget_id' => '404' ) ); ?>
+                </div>
+                <div class="widget span3">
                     <h2 class="widgettitle"><?php _e( 'Categorias mais usadas', 'odin' ); ?></h2>
                     <ul>
                         <?php wp_list_categories( array( 'orderby' => 'count', 'order' => 'DESC', 'show_count' => 1, 'title_li' => '', 'number' => 10 ) ); ?>
                     </ul>
                 </div>
-                <?php the_widget( 'WP_Widget_Archives', array( 'count' => 0, 'dropdown' => 1 ) ); ?>
-                <?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+                <div class="span3">
+                    <?php the_widget( 'WP_Widget_Archives', array( 'count' => 0, 'dropdown' => 1 ) ); ?>
+                </div>
+                <div class="span3">
+                    <?php the_widget( 'WP_Widget_Tag_Cloud' ); ?>
+                </div>
             </div><!-- .entry-content -->
         </article><!-- .error404 -->
     </div><!-- #content -->
