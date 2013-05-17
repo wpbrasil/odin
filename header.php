ed3c93e8823b2b4dd3872f89746f4ea6fae74013
claudiosmweb <contato@claudiosmweb.com>
2013-05-17 03:03:56 -0300

    fixed the navbar

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
             if ( ! empty( $header_image ) ) : ?>
                 <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><img src="<?php echo esc_url( $header_image ); ?>" height="<?php echo get_custom_header()->height; ?>" width="<?php echo get_custom_header()->width; ?>" alt="" /></a>
             <?php endif; ?>
-            <nav id="main-navigation" class="main-navigation navbar" role="navigation">
+            <nav id="main-navigation" class="navbar" role="navigation">
                 <a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Pular para o conte&uacute;do', 'odin' ); ?>"><?php _e( 'Pular para o conte&uacute;do', 'odin' ); ?></a>
                 <div class="navbar-inner">
                     <div class="container">
@@ -43,14 +43,16 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </a>
-                        <!-- <a class="brand" href="#">Project name</a> -->
+                        <?php /*
+
+                        <a class="brand" href="<?php echo home_url(); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+
+                        */ ?>
                         <div class="nav-collapse collapse">
                             <?php
                                 wp_nav_menu(
                                     array(
                                         'theme_location' => 'main-menu',
-                                        'menu'           => 'side_menu',
-                                        'depth'          => 1,
                                         'container'      => false,
                                         'menu_class'     => 'nav',
                                         'fallback_cb'    => 'wp_page_menu',
