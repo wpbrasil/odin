34e28c5f22d634945bc8edd7867c2d5e3e1c41ee
claudiosmweb <contato@claudiosmweb.com>
2013-05-26 00:56:05 -0300

    moved theodin_autoset_featured function to functions.php

--- core/thumbnails.php
+++ core/thumbnails.php
@@ -169,30 +169,3 @@ function odin_thumbnail( $width, $height, $alt, $crop = true ) {
         return apply_filters( 'odin_thumbnail_html', $html );
     }
 }
-
-/**
- * Automatically sets the post thumbnail.
- *
- * @global array $post WP post object.
- */
-function odin_autoset_featured() {
-    global $post;
-    if ( isset( $post->ID ) ) {
-        $already_has_thumb = has_post_thumbnail( $post->ID );
-        if ( ! $already_has_thumb ) {
-            $attached_image = get_children( 'post_parent=' . $post->ID . '&post_type=attachment&post_mime_type=image&numberposts=1' );
-            if ( $attached_image ) {
-                foreach ( $attached_image as $attachment_id => $attachment ) {
-                    set_post_thumbnail( $post->ID, $attachment_id );
-                }
-            }
-        }
-    }
-}
-
-add_action( 'the_post', 'odin_autoset_featured' );
-add_action( 'save_post', 'odin_autoset_featured' );
-add_action( 'draft_to_publish', 'odin_autoset_featured' );
-add_action( 'new_to_publish', 'odin_autoset_featured' );
-add_action( 'pending_to_publish', 'odin_autoset_featured' );
-add_action( 'future_to_publish', 'odin_autoset_featured' );
--- functions.php
+++ functions.php
@@ -139,6 +139,33 @@ require_once get_template_directory() . '/core/tools.php';
 require_once get_template_directory() . '/core/thumbnails.php';
 
 /**
+ * Automatically sets the post thumbnail.
+ *
+ * @global array $post WP post object.
+ */
+function odin_autoset_featured() {
+    global $post;
+    if ( isset( $post->ID ) ) {
+        $already_has_thumb = has_post_thumbnail( $post->ID );
+        if ( ! $already_has_thumb ) {
+            $attached_image = get_children( 'post_parent=' . $post->ID . '&post_type=attachment&post_mime_type=image&numberposts=1' );
+            if ( $attached_image ) {
+                foreach ( $attached_image as $attachment_id => $attachment ) {
+                    set_post_thumbnail( $post->ID, $attachment_id );
+                }
+            }
+        }
+    }
+}
+
+add_action( 'the_post', 'odin_autoset_featured' );
+add_action( 'save_post', 'odin_autoset_featured' );
+add_action( 'draft_to_publish', 'odin_autoset_featured' );
+add_action( 'new_to_publish', 'odin_autoset_featured' );
+add_action( 'pending_to_publish', 'odin_autoset_featured' );
+add_action( 'future_to_publish', 'odin_autoset_featured' );
+
+/**
  * Custom Related Posts Image.
  *
  * Use this filter for use aq_resize() in place of the_post_thumbnails().
