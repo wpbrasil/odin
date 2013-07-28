15fdcee17ad9b4095c3ba0f39c1282d4ca417fba
claudiosmweb <contato@claudiosmweb.com>
2013-07-27 21:31:47 -0300

    fixed standards in gruntfile

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -23,8 +23,8 @@ module.exports = function(grunt) {
                 "curly": true
             },
             all: [
-                'Gruntfile.js',
-                '../js/main.js'
+                "Gruntfile.js",
+                "../js/main.js"
             ]
         },
 
@@ -32,14 +32,14 @@ module.exports = function(grunt) {
         uglify: {
             dist: {
                 files: {
-                    '../js/main.min.js': [
-                        '../core/colorbox/js/*.js', // Colorbox
-                        '../core/lazyload/js/*.js', // LazyLoad
-                        // '../core/photoswipe/js/*.js', // Photoswipe
-                        '../core/socialite/js/*.js', // Socialite
-                        '../js/jquery.fitvids.min.js', // FitVids
-                        '../js/libs/*.js', // Project libs includes
-                        '../js/main.js'
+                    "../js/main.min.js": [
+                        "../core/colorbox/js/*.js", // Colorbox
+                        "../core/lazyload/js/*.js", // LazyLoad
+                        // "../core/photoswipe/js/*.js", // Photoswipe
+                        "../core/socialite/js/*.js", // Socialite
+                        "../js/jquery.fitvids.min.js", // FitVids
+                        "../js/libs/*.js", // Project libs includes
+                        "../js/main.js"
                     ]
                 }
             }
@@ -49,9 +49,9 @@ module.exports = function(grunt) {
         compass: {
             dist: {
                 options: {
-                    config: 'config.rb',
+                    config: "config.rb",
                     force: true,
-                    outputStyle: 'compressed'
+                    outputStyle: "compressed"
                 }
             }
         },
@@ -60,15 +60,15 @@ module.exports = function(grunt) {
         watch: {
             compass: {
                 files: [
-                    '../sass/**'
+                    "../sass/**"
                 ],
-                tasks: ['compass']
+                tasks: ["compass"]
             },
             js: {
                 files: [
-                    '<%= jshint.all %>'
+                    "<%= jshint.all %>"
                 ],
-                tasks: ['jshint', 'uglify']
+                tasks: ["jshint", "uglify"]
             }
         },
 
@@ -81,9 +81,9 @@ module.exports = function(grunt) {
                 },
                 files: [{
                     expand: true,
-                    cwd: '../images/',
-                    src: '../images/**',
-                    dest: '../images/'
+                    cwd: "../images/",
+                    src: "../images/**",
+                    dest: "../images/"
                 }]
             }
         },
@@ -97,13 +97,13 @@ module.exports = function(grunt) {
                 recursive: true,
                 syncDest: true,
                 exclude: [
-                    '**.DS_Store',
-                    '**Thumbs.db',
-                    '.git/',
-                    '.gitignore',
-                    'sass/',
-                    'src/',
-                    'README.md'
+                    "**.DS_Store",
+                    "**Thumbs.db",
+                    ".git/",
+                    ".gitignore",
+                    "sass/",
+                    "src/",
+                    "README.md"
                 ]
             },
             production: {
@@ -112,36 +112,36 @@ module.exports = function(grunt) {
                 host: "user@host.com",
                 recursive: true,
                 syncDest: true,
-                exclude: '<%= rsync.staging.exclude %>'
+                exclude: "<%= rsync.staging.exclude %>"
             }
         },
 
         // ftp deploy
         // ref: https://npmjs.org/package/grunt-ftp-deploy
-        'ftp-deploy': {
+        "ftp-deploy": {
             build: {
                 auth: {
-                    host: 'ftp.SEU-SITE.com',
+                    host: "ftp.SEU-SITE.com",
                     port: 21,
-                    authKey: 'key_for_deploy'
+                    authKey: "key_for_deploy"
                 },
-                src: '../',
-                dest: '/PATH/wp-content/themes/odin',
+                src: "../",
+                dest: "/PATH/wp-content/themes/odin",
                 exclusions: [
-                    '../**.DS_Store',
-                    '../**Thumbs.db',
-                    '../.git/*',
-                    '../.gitignore',
-                    '../sass/*',
-                    '../src/*',
-                    '../src/.sass-cache/*',
-                    '../src/node_modules/*',
-                    '../src/.ftppass',
-                    '../src/Gruntfile.js',
-                    '../src/config.rb',
-                    '../src/package.json',
-                    '../README.md',
-                    '../**/README.md'
+                    "../**.DS_Store",
+                    "../**Thumbs.db",
+                    "../.git/*",
+                    "../.gitignore",
+                    "../sass/*",
+                    "../src/*",
+                    "../src/.sass-cache/*",
+                    "../src/node_modules/*",
+                    "../src/.ftppass",
+                    "../src/Gruntfile.js",
+                    "../src/config.rb",
+                    "../src/package.json",
+                    "../README.md",
+                    "../**/README.md"
                 ]
             }
         },
@@ -149,36 +149,36 @@ module.exports = function(grunt) {
         // downloads dependencies
         curl: {
             bootstrap: {
-                src: 'http://twitter.github.io/bootstrap/2.3.2/assets/js/bootstrap.min.js',
-                dest: 'tmp/bootstrap.min.js'
+                src: "http://twitter.github.io/bootstrap/2.3.2/assets/js/bootstrap.min.js",
+                dest: "tmp/bootstrap.min.js"
             },
             bootstrap_sass: {
-                src: 'https://github.com/jlong/sass-twitter-bootstrap/archive/master.zip',
-                dest: 'tmp/bootstrap-sass.zip'
+                src: "https://github.com/jlong/sass-twitter-bootstrap/archive/master.zip",
+                dest: "tmp/bootstrap-sass.zip"
             }
         },
 
         // unzip files
         unzip: {
             bootstrap_scss: {
-                src: 'tmp/bootstrap-sass.zip',
-                dest: 'tmp/'
+                src: "tmp/bootstrap-sass.zip",
+                dest: "tmp/"
             }
         },
 
         // renames and moves directories and files
         rename: {
             bootstrap_scss: {
-                src: 'tmp/sass-twitter-bootstrap-master/lib',
-                dest: '../sass/bootstrap'
+                src: "tmp/sass-twitter-bootstrap-master/lib",
+                dest: "../sass/bootstrap"
             },
             bootstrap_js: {
-                src: 'tmp/bootstrap.min.js',
-                dest: '../js/bootstrap.min.js'
+                src: "tmp/bootstrap.min.js",
+                dest: "../js/bootstrap.min.js"
             },
             bootstrap_img: {
-                src: 'tmp/sass-twitter-bootstrap-master/img',
-                dest: '../images/bootstrap'
+                src: "tmp/sass-twitter-bootstrap-master/img",
+                dest: "../images/bootstrap"
             }
         },
 
@@ -202,37 +202,37 @@ module.exports = function(grunt) {
     });
 
     // load tasks
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.loadNpmTasks('grunt-contrib-imagemin');
-    grunt.loadNpmTasks('grunt-rsync');
-    grunt.loadNpmTasks('grunt-ftp-deploy');
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+    grunt.loadNpmTasks("grunt-contrib-compass");
+    grunt.loadNpmTasks("grunt-contrib-imagemin");
+    grunt.loadNpmTasks("grunt-rsync");
+    grunt.loadNpmTasks("grunt-ftp-deploy");
 
     // extra tasks
-    grunt.loadNpmTasks('grunt-curl');
-    grunt.loadNpmTasks('grunt-zip');
-    grunt.loadNpmTasks('grunt-rename');
-    grunt.loadNpmTasks('grunt-clean');
+    grunt.loadNpmTasks("grunt-curl");
+    grunt.loadNpmTasks("grunt-zip");
+    grunt.loadNpmTasks("grunt-rename");
+    grunt.loadNpmTasks("grunt-clean");
 
     // register task
-    grunt.registerTask('default', [
-        'jshint',
-        'compass',
-        'uglify'
+    grunt.registerTask("default", [
+        "jshint",
+        "compass",
+        "uglify"
     ]);
 
     // bootstrap task
-    grunt.registerTask('bootstrap', [
-        'clean:prepare',
-        'curl:bootstrap',
-        'curl:bootstrap_sass',
-        'unzip:bootstrap_scss',
-        'rename:bootstrap_scss',
-        'rename:bootstrap_js',
-        'rename:bootstrap_img',
-        'clean:bootstrap',
-        'compass'
+    grunt.registerTask("bootstrap", [
+        "clean:prepare",
+        "curl:bootstrap",
+        "curl:bootstrap_sass",
+        "unzip:bootstrap_scss",
+        "rename:bootstrap_scss",
+        "rename:bootstrap_js",
+        "rename:bootstrap_img",
+        "clean:bootstrap",
+        "compass"
     ]);
 };
