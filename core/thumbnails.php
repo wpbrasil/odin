09a107c09430bf96481b0f964ddcd6bd0dfed8cb
claudiosmweb <contato@claudiosmweb.com>
2013-08-04 23:54:34 -0300

    updated the aq_resize

--- core/thumbnails.php
+++ core/thumbnails.php
@@ -30,84 +30,110 @@ if ( ! function_exists( 'aq_resize' ) ) {
 
     function aq_resize( $url, $width = null, $height = null, $crop = null, $single = true, $upscale = false ) {
 
-        //validate inputs
+        // Validate inputs.
         if ( ! $url || ( ! $width && ! $height ) ) return false;
 
-        // caipt'n, ready to hook
-        if ( $upscale === true ) add_filter( 'image_resize_dimensions', 'aq_upscale', 10, 6 );
+        // Caipt'n, ready to hook.
+        if ( true === $upscale ) add_filter( 'image_resize_dimensions', 'aq_upscale', 10, 6 );
 
-        //define upload path & dir
+        // Define upload path & dir.
         $upload_info = wp_upload_dir();
         $upload_dir = $upload_info['basedir'];
         $upload_url = $upload_info['baseurl'];
 
-        //check if $img_url is local
-        if ( strpos( $url, $upload_url ) === false ) return false;
+        $http_prefix = "http://";
+        $https_prefix = "https://";
 
-        //define path of image
+        // if the $url scheme differs from $upload_url scheme, make them match.
+        // if the schemes differe, images don't show up.
+        if ( ! strncmp( $url, $https_prefix, strlen( $https_prefix ) ) )
+            $upload_url = str_replace( $http_prefix, $https_prefix, $upload_url );
+        elseif ( ! strncmp( $url, $http_prefix, strlen( $http_prefix ) ) )
+            $upload_url = str_replace( $https_prefix, $http_prefix, $upload_url );
+
+        // Check if $img_url is local.
+        if ( false === strpos( $url, $upload_url ) ) return false;
+
+        // Define path of image.
         $rel_path = str_replace( $upload_url, '', $url );
         $img_path = $upload_dir . $rel_path;
 
-        //check if img path exists, and is an image indeed
+        // Check if img path exists, and is an image indeed.
         if ( ! file_exists( $img_path ) or ! getimagesize( $img_path ) ) return false;
 
-        //get image info
+        // Get image info.
         $info = pathinfo( $img_path );
         $ext = $info['extension'];
         list( $orig_w, $orig_h ) = getimagesize( $img_path );
 
-        //get image size after cropping
+        // Get image size after cropping.
         $dims = image_resize_dimensions( $orig_w, $orig_h, $width, $height, $crop );
         $dst_w = $dims[4];
         $dst_h = $dims[5];
 
-        // return the original image only if it exactly fits the needed measures
-        if ( ! $dims && ( ( ( $height === null && $orig_w == $width ) xor ( $width === null && $orig_h == $height ) ) xor ( $height == $orig_h && $width == $orig_w ) ) ) {
+        // Return the original image only if it exactly fits the needed measures.
+        if ( ! $dims && ( ( ( null === $height && $orig_w == $width ) xor ( null === $width && $orig_h == $height ) ) xor ( $height == $orig_h && $width == $orig_w ) ) ) {
             $img_url = $url;
             $dst_w = $orig_w;
             $dst_h = $orig_h;
         } else {
-            //use this to check if cropped image already exists, so we can return that instead
+            // Use this to check if cropped image already exists, so we can return that instead.
             $suffix = "{$dst_w}x{$dst_h}";
             $dst_rel_path = str_replace( '.' . $ext, '', $rel_path );
             $destfilename = "{$upload_dir}{$dst_rel_path}-{$suffix}.{$ext}";
 
-            if ( ! $dims || ( $crop == true && $upscale == false && ( $dst_w < $width || $dst_h < $height ) ) ) {
-                //can't resize, so return false saying that the action to do could not be processed as planned.
+            if ( ! $dims || ( true == $crop && false == $upscale && ( $dst_w < $width || $dst_h < $height ) ) ) {
+                // Can't resize, so return false saying that the action to do could not be processed as planned.
                 return false;
             }
-            //else check if cache exists
+            // Else check if cache exists.
             elseif ( file_exists( $destfilename ) && getimagesize( $destfilename ) ) {
                 $img_url = "{$upload_url}{$dst_rel_path}-{$suffix}.{$ext}";
             }
-            //else, we resize the image and return the new resized image url
+            // Else, we resize the image and return the new resized image url.
             else {
 
-                $editor = wp_get_image_editor( $img_path );
+                // Note: This pre-3.5 fallback check will edited out in subsequent version.
+                if ( function_exists( 'wp_get_image_editor' ) ) {
 
-                if ( is_wp_error( $editor ) || is_wp_error( $editor->resize( $width, $height, $crop ) ) )
-                    return false;
+                    $editor = wp_get_image_editor( $img_path );
 
-                $resized_file = $editor->save();
+                    if ( is_wp_error( $editor ) || is_wp_error( $editor->resize( $width, $height, $crop ) ) )
+                        return false;
 
-                if ( ! is_wp_error( $resized_file ) ) {
-                    $resized_rel_path = str_replace( $upload_dir, '', $resized_file['path'] );
-                    $img_url = $upload_url . $resized_rel_path;
+                    $resized_file = $editor->save();
+
+                    if ( ! is_wp_error( $resized_file ) ) {
+                        $resized_rel_path = str_replace( $upload_dir, '', $resized_file['path'] );
+                        $img_url = $upload_url . $resized_rel_path;
+                    } else {
+                        return false;
+                    }
+
                 } else {
-                    return false;
+
+                    $resized_img_path = image_resize( $img_path, $width, $height, $crop ); // Fallback foo.
+                    if ( ! is_wp_error( $resized_img_path ) ) {
+                        $resized_rel_path = str_replace( $upload_dir, '', $resized_img_path );
+                        $img_url = $upload_url . $resized_rel_path;
+                    } else {
+                        return false;
+                    }
+
                 }
+
             }
         }
 
-        // okay, leave the ship
-        if ( $upscale === true ) remove_filter( 'image_resize_dimensions', 'aq_upscale' );
+        // Okay, leave the ship.
+        if ( true === $upscale ) remove_filter( 'image_resize_dimensions', 'aq_upscale' );
 
-        //return the output
+        // Return the output.
         if ( $single ) {
-            //str return
+            // str return.
             $image = $img_url;
         } else {
-            //array return
+            // array return.
             $image = array (
                 0 => $img_url,
                 1 => $dst_w,
@@ -118,21 +144,20 @@ if ( ! function_exists( 'aq_resize' ) ) {
         return $image;
     }
 
-    function aq_upscale( $default, $orig_w, $orig_h, $dest_w, $dest_h, $crop ) {
-        if ( ! $crop ) return null; // let the wordpress default function handle this
 
-        // here is the point we allow to use larger image size than the original one
+    function aq_upscale( $default, $orig_w, $orig_h, $dest_w, $dest_h, $crop ) {
+        if ( ! $crop ) return null; // Let the wordpress default function handle this.
+
+        // Here is the point we allow to use larger image size than the original one.
         $aspect_ratio = $orig_w / $orig_h;
         $new_w = $dest_w;
         $new_h = $dest_h;
 
-        if ( ! $new_w ) {
+        if ( ! $new_w )
             $new_w = intval( $new_h * $aspect_ratio );
-        }
 
-        if ( ! $new_h ) {
+        if ( ! $new_h )
             $new_h = intval( $new_w / $aspect_ratio );
-        }
 
         $size_ratio = max( $new_w / $orig_w, $new_h / $orig_h );
 
