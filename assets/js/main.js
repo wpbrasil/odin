b4c7d36a2c3d3c95b72bc50c16235343ae504ce3
claudiosmweb <contato@claudiosmweb.com>
2013-08-20 22:41:22 -0300

    fixed main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,4 @@
-//bxslider
 jQuery(document).ready(function($) {
-    // Slideshow.
-    // Examples in: http://bxslider.com/
-    // $('#slideshow').bxSlider();
+    // fitVids.
     $('.entry-content').fitVids();
-    $(".entry-content a[href$='.jpg'], .entry-content a[href$='.png'], .entry-content a[href$='.gif']").colorbox({
-        rel: 'gallery',
-        current: false,
-        width: "80%",
-        height: "80%"
-    });
 });
-
-
-// Photoswipe
-// Set up PhotoSwipe with all anchor tags in the Gallery container
-
-// (function(window, PhotoSwipe){
-//     document.addEventListener('DOMContentLoaded', function(){
-//         var
-//         options = {},
-//         instance = PhotoSwipe.attach( window.document.querySelectorAll('#Gallery a'), options );
-//     }, false);
-// }(window, window.Code.PhotoSwipe));
