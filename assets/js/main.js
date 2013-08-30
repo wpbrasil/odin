461cac8079efcf0fefcca79657100d2be8e2447f
claudiosmweb <contato@claudiosmweb.com>
2013-08-30 17:31:35 -0300

    updated the video shortcode styles and scripts

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,8 +3,6 @@ jQuery(document).ready(function($) {
     $(".entry-content").fitVids();
 
     // Responsive wp_video_shortcode().
-    $(".wp-video-shortcode").parent("div").css({
-        width: "auto"
-    });
+    $(".wp-video-shortcode").parent("div").css("width", "auto");
 
 });
