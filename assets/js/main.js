106f9c2f0b10d460343d91a555f4e70f71a8a4be
claudiosmweb <contato@claudiosmweb.com>
2013-08-30 17:18:01 -0300

    forced video shortcodes to be responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,10 @@
 jQuery(document).ready(function($) {
     // fitVids.
-    $('.entry-content').fitVids();
+    $(".entry-content").fitVids();
+
+    // Responsive wp_video_shortcode().
+    $(".wp-video-shortcode").parent("div").css({
+        width: "auto"
+    });
+
 });
