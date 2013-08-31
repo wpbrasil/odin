6f3cb19fe812d77d62ca9b6e2ccaec1f35081db8
claudiosmweb <contato@claudiosmweb.com>
2013-08-30 23:34:26 -0300

    added default value in Odin_Front_End_Form class #72

--- core/classes/abstracts/abstract-front-end-form.php
+++ core/classes/abstracts/abstract-front-end-form.php
@@ -81,6 +81,18 @@ abstract class Odin_Front_End_Form {
     }
 
     /**
+     * Sets the field default value.
+     *
+     * @return string Default value.
+     */
+    protected function default_field( $id, $default ) {
+        if ( 'get' == $this->method )
+            return isset( $_GET[ $id ] ) ? sanitize_text_field( $_GET[ $id ] ) : $default;
+        else
+            return isset( $_POST[ $id ] ) ? sanitize_text_field( $_POST[ $id ] ) : $default;
+    }
+
+    /**
      * Process form fields.
      *
      * @return string Form fields HTML.
@@ -102,31 +114,33 @@ abstract class Odin_Front_End_Form {
                     $description = isset( $field['description'] ) ? $field['description'] : '';
                     $attributes  = isset( $field['attributes'] ) ? $field['attributes'] : array();
                     $options     = isset( $field['options'] ) ? $field['options'] : '';
+                    $default     = isset( $field['default'] ) ? $field['default'] : '';
+                    $default     = $this->default_field( $id, $default );
 
                     switch ( $type ) {
                         case 'text':
-                            $html .= $this->field_input( $id, $label, $description, $attributes );
+                            $html .= $this->field_input( $id, $label, $default, $description, $attributes );
                             break;
                         case 'email':
-                            $html .= $this->field_input( $id, $label, $description, array_merge( array( 'type' => 'email' ), $attributes ) );
+                            $html .= $this->field_input( $id, $label, $default, $description, array_merge( array( 'type' => 'email' ), $attributes ) );
                             break;
                         case 'file':
-                            $html .= $this->field_input( $id, $label, $description, array_merge( array( 'type' => 'file', 'class' => '' ), $attributes ) );
+                            $html .= $this->field_input( $id, $label, $default, $description, array_merge( array( 'type' => 'file', 'class' => '' ), $attributes ) );
                             break;
                         case 'input':
-                            $html .= $this->field_input( $id, $label, $description, $attributes );
+                            $html .= $this->field_input( $id, $label, $default, $description, $attributes );
                             break;
                         case 'textarea':
-                            $html .= $this->field_textarea( $id, $label, $description, $attributes );
+                            $html .= $this->field_textarea( $id, $label, $default, $description, $attributes );
                             break;
                         case 'checkbox':
-                            $html .= $this->field_checkbox( $id, $label, $description, $attributes );
+                            $html .= $this->field_checkbox( $id, $label, $default, $description, $attributes );
                             break;
                         case 'select':
-                            $html .= $this->field_select( $id, $label, $description, $attributes, $options );
+                            $html .= $this->field_select( $id, $label, $default, $description, $attributes, $options );
                             break;
                         case 'radio':
-                            $html .= $this->field_radio( $id, $description, $attributes, $options );
+                            $html .= $this->field_radio( $id, $default, $description, $attributes, $options );
                             break;
 
                         default:
@@ -172,12 +186,13 @@ abstract class Odin_Front_End_Form {
      *
      * @param  string $id          Field id.
      * @param  string $label       Field label.
+     * @param  string $default     Default value.
      * @param  string $description Field description.
      * @param  array  $attributes  Array with field attributes.
      *
      * @return string              HTML of the field.
      */
-    protected function field_input( $id, $label, $description, $attributes ) {
+    protected function field_input( $id, $label, $default, $description, $attributes ) {
         // Set the default type.
         if ( ! isset( $attributes['type'] ) )
             $attributes['type'] = 'text';
@@ -188,7 +203,7 @@ abstract class Odin_Front_End_Form {
 
         $html = '<div class="form-group">';
         $html .= sprintf( '<label for="%s">%s</label>', $id, $label );
-        $html .= sprintf( '<input id="%1$s" name="%1$s"%2$s />', $id, $this->process_attributes( $attributes ) );
+        $html .= sprintf( '<input id="%1$s" name="%1$s" value="%2$s"%3$s />', $id, $default, $this->process_attributes( $attributes ) );
         $html .= ! empty( $description ) ? '<span class="help-block">' . $description . '</span>' : '';
         $html .= '</div>';
 
@@ -200,19 +215,20 @@ abstract class Odin_Front_End_Form {
      *
      * @param  string $id          Field id.
      * @param  string $label       Field label.
+     * @param  string $default     Default value.
      * @param  string $description Field description.
      * @param  array  $attributes  Array with field attributes.
      *
      * @return string              HTML of the field.
      */
-    protected function field_textarea( $id, $label, $description, $attributes ) {
+    protected function field_textarea( $id, $label, $default, $description, $attributes ) {
         // Set the default class.
         if ( ! isset( $attributes['class'] ) )
             $attributes['class'] = 'form-control';
 
         $html = '<div class="form-group">';
         $html .= sprintf( '<label for="%s">%s</label>', $id, $label );
-        $html .= sprintf( '<textarea id="%1$s" name="%1$s" cols="60" rows="4"%2$s></textarea>', $id, $this->process_attributes( $attributes ) );
+        $html .= sprintf( '<textarea id="%1$s" name="%1$s" cols="60" rows="4"%2$s>%3$s</textarea>', $id, $this->process_attributes( $attributes ), $default );
         $html .= ! empty( $description ) ? '<span class="help-block">' . $description . '</span>' : '';
         $html .= '</div>';
 
@@ -224,12 +240,17 @@ abstract class Odin_Front_End_Form {
      *
      * @param  string $id          Field id.
      * @param  string $label       Field label.
+     * @param  string $default     Default value.
      * @param  string $description Field description.
      * @param  array  $attributes  Array with field attributes.
      *
      * @return string              HTML of the field.
      */
-    protected function field_checkbox( $id, $label, $description, $attributes ) {
+    protected function field_checkbox( $id, $label, $default, $description, $attributes ) {
+        // Set the checked attribute
+        if ( ! empty( $default ) )
+            $attributes['checked'] = 'checked';
+
         $html = '<div class="checkbox">';
         $html .= '<label>';
         $html .= sprintf( '<input type="checkbox" id="%1$s" name="%1$s" value="1"%2$s />', $id, $this->process_attributes( $attributes ) );
@@ -245,13 +266,14 @@ abstract class Odin_Front_End_Form {
      *
      * @param  string $id          Field id.
      * @param  string $label       Field label.
+     * @param  string $default     Default value.
      * @param  string $description Field description.
      * @param  array  $attributes  Array with field attributes.
      * @param  array  $options     Array with field options (value => name).
      *
      * @return string              HTML of the field.
      */
-    protected function field_select( $id, $label, $description, $attributes, $options ) {
+    protected function field_select( $id, $label, $default, $description, $attributes, $options ) {
         // Set the default class.
         if ( ! isset( $attributes['class'] ) )
             $attributes['class'] = 'form-control';
@@ -260,8 +282,12 @@ abstract class Odin_Front_End_Form {
         $html .= sprintf( '<label for="%s">%s</label>', $id, $label );
         $html .= sprintf( '<select id="%1$s" name="%1$s"%2$s>', $id, $this->process_attributes( $attributes ) );
 
-        foreach ( $options as $value => $name )
-            $html .= sprintf( '<option value="%s">%s</option>', $value, $name );
+        foreach ( $options as $value => $name ) {
+            // Set the selected attribute.
+            $selected = ( $value == $default ) ? ' selected="selected"' : '';
+
+            $html .= sprintf( '<option value="%s"%s>%s</option>', $value, $selected, $name );
+        }
 
         $html .= '</select>';
         $html .= ! empty( $description ) ? '<span class="help-block">' . $description . '</span>' : '';
@@ -274,16 +300,23 @@ abstract class Odin_Front_End_Form {
      * Radio field.
      *
      * @param  string $id          Field id.
+     * @param  string $default     Default value.
      * @param  string $description Field description.
      * @param  array  $attributes  Array with field attributes.
      * @param  array  $options     Array with field options (value => label).
      *
      * @return string              HTML of the field.
      */
-    protected function field_radio( $id, $description, $attributes, $options ) {
+    protected function field_radio( $id, $default, $description, $attributes, $options ) {
         $html = '';
 
         foreach ( $options as $value => $label ) {
+            // Set the checked attribute.
+            if ( $value == $default )
+                $attributes['checked'] = 'checked';
+            else if ( isset( $attributes['checked'] ) )
+                unset( $attributes['checked'] );
+
             $html .= '<div class="radio">';
             $html .= sprintf( '<label><input type="radio" id="%1$s-%2$s" name="%1$s" value="%2$s"%4$s /> %3$s</label>', $id, $value, $label, $this->process_attributes( $attributes ) );
             $html .= '</div>';
@@ -316,4 +349,5 @@ abstract class Odin_Front_End_Form {
 
         return $html;
     }
+
 }
