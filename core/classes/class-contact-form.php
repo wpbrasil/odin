4d1c1ecdfb45d69064e90b6f32fb98b9ad1d621e
claudiosmweb <contato@claudiosmweb.com>
2013-08-30 22:56:07 -0300

    created a initial Odin_Contact_Form class

---
+++ core/classes/class-contact-form.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Odin_Contact_Form class.
+ *
+ * Built Contact Forms.
+ *
+ * @package  Odin
+ * @category Contact Form
+ * @author   WPBrasil
+ * @version  2.0.0
+ */
+class Odin_Contact_Form extends Odin_Front_End_Form {
+
+}
