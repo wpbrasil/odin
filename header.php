8b8692a4d00e47f94f0536f0efe4bdaf4318e903
claudiosmweb <contato@claudiosmweb.com>
2013-09-16 17:57:07 -0300

    fix #81

--- header.php
+++ header.php
@@ -29,10 +29,8 @@
     <div class="container">
         <header id="header" role="banner">
             <?php if ( is_home() ) : ?>
-                <hgroup>
-                    <h1 class="site-title"><a href="<?php echo home_url(); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-                    <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-                </hgroup>
+                <h1 class="site-title"><a href="<?php echo home_url(); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                <h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
             <?php else: ?>
                 <div class="site-title h1"><a href="<?php echo home_url(); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></div>
                 <div class="site-description h2"><?php bloginfo( 'description' ); ?></div>
