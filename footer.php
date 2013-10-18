00c2bb14c8178e6d06726617663841c0679286e3
adammacias <eu@adammacias.com>
2013-10-18 04:16:21 -0300

    fixed link odin

--- footer.php
+++ footer.php
@@ -11,7 +11,7 @@
 ?>
     </div><!-- #main -->
     <footer id="footer" role="contentinfo">
-        <span>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a> - <?php _e( 'All rights reserved', 'odin' ); ?> | <?php echo sprintf( __( 'Powered by the <a href="%s" rel="nofollow" target="_blank" itemprop="copyrightHolder">Odin</a> forces and <a href="%s" rel="nofollow" target="_blank">WordPress</a>.', 'odin' ), 'http://wpbrasil.github.com/odin', 'http://wordpress.org/' ); ?></span>
+        <span>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo home_url(); ?>"><?php bloginfo( 'name' ); ?></a> - <?php _e( 'All rights reserved', 'odin' ); ?> | <?php echo sprintf( __( 'Powered by the <a href="%s" rel="nofollow" target="_blank" itemprop="copyrightHolder">Odin</a> forces and <a href="%s" rel="nofollow" target="_blank">WordPress</a>.', 'odin' ), 'http://wpod.in/', 'http://wordpress.org/' ); ?></span>
     </footer><!-- #footer -->
 </div><!-- .container -->
 <?php wp_footer(); ?>
