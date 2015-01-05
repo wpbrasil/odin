e0dcb4342c8735ca24f9cd5418684c5489b1d3b5
adammacias <eu@adammacias.com>
2015-01-05 21:20:37 -0200

    add theme support HTML5 #226

--- header.php
+++ header.php
@@ -81,7 +81,7 @@
 					<form method="get" class="navbar-form navbar-right" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
 						<label for="navbar-search" class="sr-only"><?php _e( 'Search:', 'odin' ); ?></label>
 						<div class="form-group">
-							<input type="text" class="form-control" name="s" id="navbar-search" />
+							<input type="search" class="form-control" name="s" id="navbar-search" />
 						</div>
 						<button type="submit" class="btn btn-default"><?php _e( 'Search', 'odin' ); ?></button>
 					</form>
