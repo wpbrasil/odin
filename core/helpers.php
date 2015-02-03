7981a7cc860f8858ef26d6d976900ca5f1720650
Rodrigo Brito <rig.10@hotmail.com>
2015-02-03 14:50:25 -0200

    Upscale em odin_thumbnail
    Inclusão do parâmetro upscale para forçar o redimensionamento em imagens
    menores que os valores de corte.

--- core/helpers.php
+++ core/helpers.php
@@ -395,16 +395,18 @@ function odin_breadcrumbs( $homepage = '' ) {
 /**
  * Get a image URL.
  *
- * @param  int $id     Image ID.
- * @param  int $width  Image width.
- * @param  int $height Image height.
+ * @param  int 		$id   		Image ID.
+ * @param  int 		$width   	Image width.
+ * @param  int 		$height  	Image height.
+ * @param  boolean 	$crop 	 	Image crop.
+ * @param  boolean 	$upscale 	Force the resize.
  *
  * @return string
  */
-function odin_get_image_url( $id, $width, $height, $crop = true ) {
+function odin_get_image_url( $id, $width, $height, $crop = true, $upscale = false ) {
 	$resizer    = Odin_Thumbnail_Resizer::get_instance();
 	$origin_url = wp_get_attachment_url( $id );
-	$url        = $resizer->process( $origin_url, $width, $height, $crop );
+	$url        = $resizer->process( $origin_url, $width, $height, $crop, $upscale );
 
 	return $url;
 }
@@ -414,16 +416,17 @@ function odin_get_image_url( $id, $width, $height, $crop = true ) {
  *
  * @since  2.2.0
  *
- * @param  int     $width  Width of the image.
- * @param  int     $height Height of the image.
- * @param  string  $class  Class attribute of the image.
- * @param  string  $alt    Alt attribute of the image.
- * @param  bool    $crop   Image crop.
- * @param  string  $class  Custom HTML classes.
+ * @param  int     $width  	Width of the image.
+ * @param  int     $height 	Height of the image.
+ * @param  string  $class  	Class attribute of the image.
+ * @param  string  $alt    	Alt attribute of the image.
+ * @param  boolean $crop   	Image crop.
+ * @param  boolean $upscale	Force the resize.
+ * @param  string  $class  	Custom HTML classes.
  *
  * @return string         Return the post thumbnail.
  */
-function odin_thumbnail( $width, $height, $alt, $crop = true, $class = '' ) {
+function odin_thumbnail( $width, $height, $alt, $crop = true, $upscale = false, $class = '' ) {
 	if ( ! class_exists( 'Odin_Thumbnail_Resizer' ) ) {
 		return;
 	}
@@ -431,7 +434,7 @@ function odin_thumbnail( $width, $height, $alt, $crop = true, $class = '' ) {
 	$thumb = get_post_thumbnail_id();
 
 	if ( $thumb ) {
-		$image = odin_get_image_url( $thumb, $width, $height, $crop );
+		$image = odin_get_image_url( $thumb, $width, $height, $crop, $upscale );
 		$html  = '<img class="wp-image-thumb img-responsive ' . sanitize_html_class( $class ) . '" src="' . $image . '" width="' . esc_attr( $width ) . '" height="' . esc_attr( $height ) . '" alt="' . esc_attr( $alt ) . '" />';
 
 		return apply_filters( 'odin_thumbnail_html', $html );
