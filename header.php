6b2b78dda47a1103043600066883fbd0ffd35090
adammacias <eu@adammacias.com>
2015-02-16 17:38:10 -0200

    Removido hack IE 7 e 8 #243

--- header.php
+++ header.php
@@ -8,15 +8,7 @@
  * @since 2.2.0
  */
 ?><!DOCTYPE html>
-<!--[if IE 7]>
-<html class="no-js ie ie7 lt-ie9 lt-ie8" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if IE 8]>
-<html class="no-js ie ie8 lt-ie9" <?php language_attributes(); ?>>
-<![endif]-->
-<!--[if !(IE 7) | !(IE 8) ]><!-->
 <html class="no-js" <?php language_attributes(); ?>>
-<!--<![endif]-->
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
