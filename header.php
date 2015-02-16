5983291997fb2cceabfc8cfe99df4ed481ff82a7
adammacias <eu@adammacias.com>
2015-02-16 18:05:01 -0200

    Removido Hack IE #243

--- header.php
+++ header.php
@@ -11,7 +11,6 @@
 <html class="no-js" <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
