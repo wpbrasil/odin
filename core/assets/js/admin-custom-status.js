74fac056da500cde3f2524bbf589280765d7bc92
matheus <contato@matheusgimenez.com.br>
2015-07-11 20:06:11 -0300

    remove DOM manipulation in forEach

--- core/assets/js/admin-custom-status.js
+++ core/assets/js/admin-custom-status.js
@@ -5,7 +5,10 @@
 	 * Custom post status
 	 */
 	$( window ).load( function() {
+		var html = '';
+		var label = false;
 		var $odinMeta = document.querySelectorAll( 'meta.odin-custom-status-meta' );
+
 		Array.prototype.forEach.call( $odinMeta, function( item ) {
 			var $meta = $( item );
 			var args = $.parseJSON( $meta.attr( 'value' ) );
@@ -13,20 +16,26 @@
 			if( $( document.body ).hasClass( 'post-php' ) || $( document.body ).hasClass( 'post-new-php' ) ) {
 				var select = '';
 				if( args.select ) {
-					console.log('ahoy!');
 					select = 'selected="selected"';
-					$( 'label[for="post_status"]' ).append( '<span id="post-status-display">&nbsp;' + $.trim( args.appliedLabel ) + '</span>' );
+					label = '<span id="post-status-display">&nbsp;' + $.trim( args.appliedLabel ) + '</span>';
 				}
-				var html = '<option value="' + $.trim( args.slug ) + '" ' + $.trim( select ) + '>' + $.trim( args.appliedLabel ) + '</option>';
-				$( '#post_status' ).append( html );
+				html += '<option value="' + $.trim( args.slug ) + '" ' + $.trim( select ) + '>' + $.trim( args.appliedLabel ) + '</option>';
 			}
 			if( $( document.body ).hasClass( 'edit-php' ) ) {
-				var html = '<option value="' + $.trim( args.slug ) + '">' + $.trim( args.appliedLabel ) + '</option>';
-				var $inlineStatus = document.querySelectorAll( '.inline-edit-status select' );
-				Array.prototype.forEach.call( $inlineStatus, function( item ) {
-					$( item ).append( html );
-				});
+				html += '<option value="' + $.trim( args.slug ) + '">' + $.trim( args.appliedLabel ) + '</option>';
 			}
 		});
+		if( $( document.body ).hasClass( 'post-php' ) || $( document.body ).hasClass( 'post-new-php' ) ) {
+			$( '#post_status ').append( html );
+			if( label ) {
+				$( 'label[for="post_status"]' ).append( label );
+			}
+		}
+		if( $( document.body ).hasClass( 'edit-php' ) ) {
+			var $inlineStatus = document.querySelectorAll( '.inline-edit-status select' );
+			Array.prototype.forEach.call( $inlineStatus, function( item ) {
+				$( item ).append( html );
+			});
+		}
 	});
 }( jQuery ));
