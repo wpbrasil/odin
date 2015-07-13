d03a7ac9049ea852cad22e1c599fddb1954e309d
AdsonCicilioti <eu@adsonagencia.com>
2015-07-13 16:28:12 -0300

    Placeholder for post without thumb
    Placeholder for posts without Featured Image in odin_related_posts
    function.

--- core/helpers.php
+++ core/helpers.php
@@ -158,8 +158,13 @@ function odin_related_posts( $display = 'category', $qty = 4, $title = '', $thum
 				$layout .= ( $thumb ) ? '<div class="col-md-' . ceil( 12 / $qty ) . '">' : '<li>';
 
 				if ( $thumb ) {
+					if ( has_post_thumbnail() ) {
+						$image = get_the_post_thumbnail( get_the_ID(), 'thumbnail' )
+					} else {
+						$image = '<img src="' . get_template_directory_uri() . '/core/assets/images/odin-thumb-placeholder.jpg" alt="' . get_the_title() . '">';
+					}
 					// Filter to replace the image.
-					$image = apply_filters( 'odin_related_posts_thumbnail', get_the_post_thumbnail( get_the_ID(), 'thumbnail' ) );
+					apply_filters( 'odin_related_posts_thumbnail', $image );
 
 					$layout .= '<span class="thumb">';
 					$layout .= sprintf( '<a href="%s" title="%s" class="thumbnail">%s</a>', get_permalink(), get_the_title(), $image );
