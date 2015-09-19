9975b3a86586ed242c92a339e4483909c829873e
tiagofabre <tiagofabre@gmail.com>
2015-09-19 00:11:56 -0300

    desabilitando por padrão o menu de shortcodes

--- functions.php
+++ functions.php
@@ -25,13 +25,13 @@ if ( ! isset( $content_width ) ) {
  */
 require_once get_template_directory() . '/core/classes/class-bootstrap-nav.php';
 require_once get_template_directory() . '/core/classes/class-shortcodes.php';
-require_once get_template_directory() . '/core/classes/class-shortcodes-menu.php';
+//require_once get_template_directory() . '/core/classes/class-shortcodes-menu.php';
 require_once get_template_directory() . '/core/classes/class-thumbnail-resizer.php';
 // require_once get_template_directory() . '/core/classes/class-theme-options.php';
 // require_once get_template_directory() . '/core/classes/class-options-helper.php';
 // require_once get_template_directory() . '/core/classes/class-post-type.php';
 // require_once get_template_directory() . '/core/classes/class-taxonomy.php';
-//require_once get_template_directory() . '/core/classes/class-metabox.php';
+// require_once get_template_directory() . '/core/classes/class-metabox.php';
 // require_once get_template_directory() . '/core/classes/abstracts/abstract-front-end-form.php';
 // require_once get_template_directory() . '/core/classes/class-contact-form.php';
 // require_once get_template_directory() . '/core/classes/class-post-form.php';
