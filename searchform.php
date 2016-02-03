011662bb9b84c0f011712ade7066e9a5b75ab49a
Rodrigo Brito <rig.10@hotmail.com>
2016-02-03 16:23:58 -0200

    Fix searchform

--- searchform.php
+++ searchform.php
@@ -8,9 +8,12 @@
 ?>
 
 <form method="get" id="searchform" class="form-inline" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-	<div class="form-group">
-		<label for="s" class="sr-only"><?php _e( 'Search', 'odin' ); ?></label>
-		<input type="search" class="form-control" name="s" id="s" value="<?php echo get_search_query(); ?>" />
-	</div>
-	<input type="submit" class="btn btn-default" value="<?php esc_attr_e( 'Search', 'odin' ); ?>" />
-</form>
+	<div class="input-group">
+		<input type="search" class="form-control" name="s" id="s" value="<?php echo get_search_query(); ?>" placeholder="<?php _e( 'Search', 'odin' ); ?>" />
+		<span class="input-group-btn">
+			<button type="submit" class="btn btn-default" value="<?php esc_attr_e( 'Search', 'odin' ); ?>">
+				<i class="glyphicon glyphicon-search"></i>
+			</button>
+		</span><!-- /input-group-btn -->
+    </div><!-- /input-group -->
+</form><!-- /searchform -->
