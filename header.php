992905069dc1babd5f782c2d10d4c0fcfc952a53
adammacias <eu@adammacias.com>
2016-05-17 17:34:12 -0300

    Remove favicon

--- header.php
+++ header.php
@@ -14,9 +14,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	<?php if ( ! get_option( 'site_icon' ) ) : ?>
-		<link href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.ico" rel="shortcut icon" />
-	<?php endif; ?>
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/html5.js"></script>
 	<![endif]-->
