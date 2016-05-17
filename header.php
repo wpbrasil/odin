4da5a258080bc9a6e39d2635f5f775d98ff5988b
adammacias <eu@adammacias.com>
2016-05-17 18:10:44 -0300

    Remove script html5.js

--- header.php
+++ header.php
@@ -14,9 +14,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	<!--[if lt IE 9]>
-	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/html5.js"></script>
-	<![endif]-->
 	<?php wp_head(); ?>
 </head>
 
