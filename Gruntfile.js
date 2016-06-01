1067c02e967b0d70c9c7d3ce9998889917b67b85
adammacias <eu@adammacias.com>
2016-06-01 08:26:42 -0300

    Update .jshintrc to main directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,7 +12,7 @@ module.exports = function( grunt ) {
 		// javascript linting with jshint
 		jshint: {
 			options: {
-				jshintrc: '<%= pkg.dirs.js %>/.jshintrc'
+				jshintrc: '.jshintrc'
 			},
 			all: [
 				'Gruntfile.js',
