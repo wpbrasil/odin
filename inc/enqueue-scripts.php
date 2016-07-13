c2c1a4cfd215a98879cd73e2fc4afc777a50600c
adammacias <eu@adammacias.com>
2016-07-13 00:21:26 -0300

    Update enqueue_scripts() function

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -1,38 +1,37 @@
 <?php
+
 /**
- * Load site scripts.
+ * Load scripts and styles.
  *
  * @since 2.2.0
  */
 function odin_enqueue_scripts() {
 	$template_url = get_template_directory_uri();
 
-	// Odin main stylesheet.
-	wp_enqueue_style( 'odin-style', get_stylesheet_uri(), array(), null, 'all' );
-
-	// jQuery script.
+	// JQuery script.
 	wp_enqueue_script( 'jquery' );
 
 	// IE-specific scripts with conditional comments.
 	wp_enqueue_script( 'respondjs', 'https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js' );
 	wp_script_add_data( 'respondjs', 'conditional', 'lt IE 9' );
 
-	wp_enqueue_script( 'html5shiv', 'https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js');
+	wp_enqueue_script( 'html5shiv', 'https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js' );
 	wp_script_add_data( 'html5shiv', 'conditional', 'lt IE 9' );
 
 	// General scripts.
 	if ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) {
-		// Bower with vendors (Bootstrap, FitVids and etc).
-		wp_enqueue_script( 'bootstrap', $template_url . '/assets/js/bower.js', array(), null, true );
+		// Loads main stylesheet file.
+		wp_enqueue_style( 'odin-main-style', $template_url . '/assets/css/main.css' );
 
-		// Main jQuery.
-		wp_enqueue_script( 'odin-main', $template_url . '/assets/js/main.js', array(), null, true );
+		// Loads main script file.
+		wp_enqueue_script( 'odin-main-script', $template_url . '/assets/js/main.js', array(), null, true );
 
-		// Grunt watch livereload in the browser.
-		// wp_enqueue_script( 'odin-livereload', 'http://localhost:35729/livereload.js?snipver=1', array(), null, true );
 	} else {
-		// Grunt main file with Bootstrap, FitVids and others libs.
-		wp_enqueue_script( 'odin-main-min', $template_url . '/assets/js/main.min.js', array(), null, true );
+		// Loads main stylesheet file compressed.
+		wp_enqueue_style( 'odin-main-style', get_stylesheet_uri() );
+
+		// Loads main script file compressed.
+		wp_enqueue_script( 'odin-main-script', $template_url . '/assets/js/main.min.js', array(), null, true );
 	}
 
 	// Load Thread comments WordPress script.
@@ -54,7 +53,7 @@ add_action( 'wp_enqueue_scripts', 'odin_enqueue_scripts', 1 );
  * @return string      New URI.
  */
 function odin_stylesheet_uri( $uri, $dir ) {
-	return $dir . '/assets/css/style.css';
+	return $dir . '/assets/css/main.min.css';
 }
 
 add_filter( 'stylesheet_uri', 'odin_stylesheet_uri', 10, 2 );
