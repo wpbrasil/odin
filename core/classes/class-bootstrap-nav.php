e9dde92600a073b0316f277be794694293c3dd53
Mario Souto <mariosoutoskn@gmail.com>
2016-12-05 11:41:40 -0200

    Additing strip tags to the title of the menu link
    The strip tags removes any tag that could be on the title and solve SEO and acessibility issues.

--- core/classes/class-bootstrap-nav.php
+++ core/classes/class-bootstrap-nav.php
@@ -77,7 +77,7 @@ class Odin_Bootstrap_Nav_Walker extends Walker_Nav_Menu {
 			$output .= $indent . '<li' . $id . $value . $class_names .'>';
 
 			$atts = array();
-			$atts['title']  = ! empty( $item->title ) ? $item->title : '';
+			$atts['title']  = ! empty( $item->title ) ? strip_tags( $item->title ) : '';
 			$atts['target'] = ! empty( $item->target ) ? $item->target : '';
 			$atts['rel']    = ! empty( $item->xfn ) ? $item->xfn : '';
 
