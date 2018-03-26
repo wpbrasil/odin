54f363d3143459d72fd4ade8693a02651a5539c0
Adam Macias <eu@adammacias.com>
2018-03-26 12:04:36 -0300

    Update meta tag viewport bootstrap 4

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<?php if ( ! get_option( 'site_icon' ) ) : ?>
