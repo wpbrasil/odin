60fe88d15efb5d1208d480fd443f3788bdd1f6ed
Adam Macias <eu@adammacias.com>
2018-03-31 17:54:48 -0300

    Update navigation-main component

--- components/navigation/navigation-main.php
+++ components/navigation/navigation-main.php
@@ -12,22 +12,26 @@
 
 <nav class="odin-navMain" role="navigation" id="menu">
 
-	<button class="odin-navMain__toggler" type="button" data-toggle="collapse" data-target="#MainMenuCollapse" aria-controls="MainMenuCollapse" aria-expanded="false" aria-label="<?php esc_html_e( 'Toggle navigation', 'odin' ); ?>">
-		<span></span>
-	</button>
+	<div class="odin-navMain-wrapper">
 
-	<div class="odin-navMain__collapse" id="MainMenuCollapse">
+		<button class="odin-navMain__toggler" type="button" data-toggle="collapse" data-target="#MainMenuCollapse" aria-controls="MainMenuCollapse" aria-expanded="false" aria-label="<?php esc_html_e( 'Toggle navigation', 'odin' ); ?>">
+			<span></span>
+		</button>
 
-		<?php
-		/**
-		 * Main menu.
-		 */
-		wp_nav_menu( array(
-			'theme_location' => 'menu-main',
-			'menu_class'     => 'odin-navMain__mainMenu',
-			'container'      => 'ul',
-			'depth'          => 2,
-		) ); ?>
+		<div class="odin-navMain__collapse" id="MainMenuCollapse">
+
+			<?php
+			/**
+			 * Main menu.
+			 */
+			wp_nav_menu( array(
+				'theme_location' => 'menu-main',
+				'menu_class'     => 'odin-navMain__mainMenu',
+				'container'      => 'ul',
+				'depth'          => 2,
+			) ); ?>
+
+		</div>
 
 	</div>
 
