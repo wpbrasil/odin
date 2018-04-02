d35fc90a6c6d8919c150ee36e6da21edbbfd7ae3
Adam Macias <eu@adammacias.com>
2018-04-02 16:58:05 -0300

    Update form search component

--- searchform.php
+++ searchform.php
@@ -1,19 +1,12 @@
 <?php
+
 /**
- * The template for displaying Search Form.
+ * The template for displaying search form.
+ *
+ * @link https://developer.wordpress.org/reference/functions/get_search_form
  *
  * @package Odin
- * @since 2.2.0
+ * @subpackage  Custom_Theme
  */
-?>
 
-<form method="get" id="searchform" class="form-inline" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-	<div class="input-group">
-		<input type="search" class="form-control" name="s" id="s" value="<?php echo get_search_query(); ?>" placeholder="<?php esc_attr_e( 'Search', 'odin' ); ?>" />
-		<span class="input-group-btn">
-			<button type="submit" class="btn btn-default" value="<?php esc_attr_e( 'Search', 'odin' ); ?>">
-				<i class="glyphicon glyphicon-search"></i>
-			</button>
-		</span><!-- /input-group-btn -->
-    </div><!-- /input-group -->
-</form><!-- /searchform -->
+get_template_part( 'components/form/form', 'search' );
