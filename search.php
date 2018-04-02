e2193f210ca717d8026befb6369982077890bcf0
Adam Macias <eu@adammacias.com>
2018-04-02 19:41:38 -0300

    Update search template

--- search.php
+++ search.php
@@ -1,45 +1,29 @@
 <?php
 /**
- * The template for displaying Search Results pages.
+ * The template for displaying search results pages.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
  *
  * @package Odin
- * @since 2.2.0
+ * @subpackage Custom_Theme
  */
 
-get_header(); ?>
+/**
+ * Header Component.
+ */
+get_header();
 
-	<main id="content" class="<?php echo odin_classes_page_sidebar(); ?>" tabindex="-1" role="main">
-			<?php if ( have_posts() ) : ?>
+/**
+ * Main Component.
+ */
+get_template_part( 'components/main/main' );
 
-				<header class="page-header">
-					<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'odin' ), get_search_query() ); ?></h1>
-				</header><!-- .page-header -->
-
-					<?php
-						// Start the Loop.
-						while ( have_posts() ) : the_post();
-
-							/*
-							 * Include the post format-specific template for the content. If you want to
-							 * use this in a child theme, then include a file called content-___.php
-							 * (where ___ is the post format) and that will be used instead.
-							 */
-							get_template_part( 'content', get_post_format() );
-
-						endwhile;
-
-						// Post navigation.
-						odin_paging_nav();
-
-					else :
-						// If no content, include the "No posts found" template.
-						get_template_part( 'content', 'none' );
-
-				endif;
-			?>
-
-	</main><!-- #main -->
-
-<?php
+/**
+ * Sidebar Component.
+ */
 get_sidebar();
+
+/**
+ * Footer Component.
+ */
 get_footer();
