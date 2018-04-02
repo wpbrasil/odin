1f106b4194f5c5d8bf28b3519857beb5a153209f
Adam Macias <eu@adammacias.com>
2018-04-02 19:40:29 -0300

    Update page sidebar template

--- page-sidebar.php
+++ page-sidebar.php
@@ -1,34 +1,31 @@
 <?php
 /**
- * Template Name: With Sidebar
+ * Template Name: Page Sidebar
  *
  * The template for displaying pages with sidebar.
  *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
  * @package Odin
- * @since 2.2.0
+ * @subpackage Custom_Theme
  */
 
+/**
+ * Header Component.
+ */
 get_header();
-?>
 
-	<main id="content" class="<?php echo odin_classes_page_sidebar(); ?>" tabindex="-1" role="main">
+/**
+ * Content Component.
+ */
+get_template_part( 'components/main/main', 'page' );
 
-			<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
-
-					// Include the page content template.
-					get_template_part( 'content', 'page' );
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				endwhile;
-			?>
-
-	</main><!-- #main -->
-
-<?php
+/**
+ * Sidebar Component.
+ */
 get_sidebar();
+
+/**
+ * Footer Component.
+ */
 get_footer();
