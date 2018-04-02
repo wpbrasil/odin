15c4c5c68d41b044da9e1597e7aa4358c5fe310d
Adam Macias <eu@adammacias.com>
2018-04-02 16:56:47 -0300

    Update sidebar component

---
+++ components/sidebar/sidebar.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Sidebar component.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Odin
+ * @subpackage Custom_Theme
+ */
+
+?>
+
+<aside class="odin-sidebar" id="secundary" role="complementary">
+
+	<div class="odin-sidebar-wrapper">
+
+		<?php
+		if ( ! dynamic_sidebar( 'main-sidebar' ) ) {
+			the_widget( 'WP_Widget_Recent_Posts', array( 'number' => 10 ) );
+			the_widget( 'WP_Widget_Archives', array( 'count' => 0, 'dropdown' => 1 ) );
+			the_widget( 'WP_Widget_Tag_Cloud' );
+		}
+		?>
+
+	</div>
+
+</aside><!-- .odin-main__sidebar -->
--- sidebar.php
+++ sidebar.php
@@ -1,20 +1,14 @@
 <?php
 /**
- * The sidebar containing the secondary widget area, displays on homepage, archives and posts.
+ * The sidebar containing the main widget area.
  *
- * If no active widgets in this sidebar, it will shows Recent Posts, Archives and Tag Cloud widgets.
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
  * @package Odin
- * @since 2.2.0
+ * @subpackage Custom_Theme
  */
-?>
 
-<aside id="sidebar" class="<?php echo odin_classes_page_sidebar_aside(); ?>" role="complementary">
-	<?php
-		if ( ! dynamic_sidebar( 'main-sidebar' ) ) {
-			the_widget( 'WP_Widget_Recent_Posts', array( 'number' => 10 ) );
-			the_widget( 'WP_Widget_Archives', array( 'count' => 0, 'dropdown' => 1 ) );
-			the_widget( 'WP_Widget_Tag_Cloud' );
-		}
-	?>
-</aside><!-- #sidebar -->
+/**
+ * Sidebar component.
+ */
+get_template_part( 'components/sidebar/sidebar' );
