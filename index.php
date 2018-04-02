eedeeb25da96d9ec140671abcadcb07c3cd28db9
Adam Macias <eu@adammacias.com>
2018-04-02 19:39:55 -0300

    Update index template

--- index.php
+++ index.php
@@ -2,47 +2,33 @@
 /**
  * The main template file.
  *
- * This is the most generic template file in a WordPress theme and one of the
- * two required files for a theme (the other being style.css).
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
  * It is used to display a page when nothing more specific matches a query.
- * For example, it puts together the home page when no home.php file exists.
+ * E.g., it puts together the home page when no home.php file exists.
  *
- * @link http://codex.wordpress.org/Template_Hierarchy
+ * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package Odin
- * @since 2.2.0
+ * @subpackage Custom_Theme
  */
 
-get_header(); ?>
+/**
+ * Header Component.
+ */
+get_header();
 
-	<main id="content" class="<?php echo odin_classes_page_sidebar(); ?>" tabindex="-1" role="main">
+/**
+ * Main Component.
+ */
+get_template_part( 'components/main/main' );
 
-			<?php
-				if ( have_posts() ) :
-					// Start the Loop.
-					while ( have_posts() ) : the_post();
-
-						/*
-						 * Include the post format-specific template for the content. If you want to
-						 * use this in a child theme, then include a file called content-___.php
-						 * (where ___ is the post format) and that will be used instead.
-						 */
-						get_template_part( 'content', get_post_format() );
-
-					endwhile;
-
-					// Post navigation.
-					odin_paging_nav();
-
-				else :
-					// If no content, include the "No posts found" template.
-					get_template_part( 'content', 'none' );
-
-				endif;
-			?>
-
-	</main><!-- #content -->
-
-<?php
+/**
+ * Sidebar Component.
+ */
 get_sidebar();
+
+/**
+ * Footer Component.
+ */
 get_footer();
