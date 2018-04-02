c475e1f0d23633832ef2b608e1995f536d5a2281
Adam Macias <eu@adammacias.com>
2018-04-02 19:40:12 -0300

    Update page template

--- page.php
+++ page.php
@@ -3,32 +3,27 @@
  * The template for displaying all pages.
  *
  * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that other
- * 'pages' on your WordPress site will use a different template.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package Odin
- * @since 2.2.0
+ * @subpackage Custom_Theme
  */
 
-get_header(); ?>
+/**
+ * Header Component.
+ */
+get_header();
 
-	<main id="content" class="<?php echo odin_classes_page_full(); ?>" tabindex="-1" role="main">
+/**
+ * Main Page Component.
+ */
+get_template_part( 'components/main/main', 'page' );
 
-			<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
-
-					// Include the page content template.
-					get_template_part( 'content', 'page' );
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				endwhile;
-			?>
-
-	</main><!-- #main -->
-
-<?php
+/**
+ * Footer Component.
+ */
 get_footer();
