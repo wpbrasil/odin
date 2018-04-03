7db686b2c395463ab145aef48aa925b6be325a2c
Adam Macias <eu@adammacias.com>
2018-04-03 16:05:50 -0300

    Update archive template

--- archive.php
+++ archive.php
@@ -1,59 +1,29 @@
 <?php
 /**
- * The template for displaying Archive pages.
+ * The template for displaying archive pages.
  *
- * Used to display archive-type pages if nothing more specific matches a query.
- * For example, puts together date-based pages if no date.php file exists.
- *
- * If you'd like to further customize these archive views, you may create a
- * new template file for each specific one. For example, Twenty Thirteen
- * already has tag.php for Tag archives, category.php for Category archives,
- * and author.php for Author archives.
- *
- * @link http://codex.wordpress.org/Template_Hierarchy
+ * @link https://codex.wordpress.org/Template_Hierarchy
  *
  * @package Odin
- * @since 2.2.0
+ * @subpackage Custom_Theme
  */
 
-get_header(); ?>
+/**
+ * Header Component.
+ */
+get_header();
 
-	<main id="content" class="<?php echo odin_classes_page_sidebar(); ?>" tabindex="-1" role="main">
+/**
+ * Main Component.
+ */
+get_template_part( 'components/main/main' );
 
-			<?php if ( have_posts() ) : ?>
-
-				<header class="page-header">
-					<?php
-						the_archive_title( '<h1 class="page-title">', '</h1>' );
-						the_archive_description( '<div class="taxonomy-description">', '</div>' );
-					?>
-				</header><!-- .page-header -->
-
-				<?php
-					// Start the Loop.
-					while ( have_posts() ) : the_post();
-
-						/*
-						 * Include the post format-specific template for the content. If you want to
-						 * use this in a child theme, then include a file called content-___.php
-						 * (where ___ is the post format) and that will be used instead.
-						 */
-						get_template_part( 'content', get_post_format() );
-
-					endwhile;
-
-					// Page navigation.
-					odin_paging_nav();
-
-				else :
-					// If no content, include the "No posts found" template.
-					get_template_part( 'content', 'none' );
-
-				endif;
-			?>
-
-	</main><!-- #main -->
-
-<?php
+/**
+ * Sidebar Component.
+ */
 get_sidebar();
+
+/**
+ * Footer Component.
+ */
 get_footer();
