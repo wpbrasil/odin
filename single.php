9d6518cd83ba8b05966e939bfc56144a113bd4be
Adam Macias <eu@adammacias.com>
2018-04-03 17:38:58 -0300

    Update single template

--- single.php
+++ single.php
@@ -1,33 +1,29 @@
 <?php
 /**
- * The Template for displaying all single posts.
+ * The template for displaying all single posts.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
  * @package Odin
- * @since 2.2.0
+ * @subpackage Custom_Theme
  */
 
-get_header(); ?>
+/**
+ * Header Component.
+ */
+get_header();
 
-		<main id="content" class="<?php echo odin_classes_page_sidebar(); ?>" tabindex="-1" role="main">
-			<?php
-				// Start the Loop.
-				while ( have_posts() ) : the_post();
+/**
+ * Main Component.
+ */
+get_template_part( 'components/main/main' );
 
-					/*
-					 * Include the post format-specific template for the content. If you want to
-					 * use this in a child theme, then include a file called content-___.php
-					 * (where ___ is the post format) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				endwhile;
-			?>
-		</main><!-- #main -->
-
-<?php
+/**
+ * Sidebar Component.
+ */
 get_sidebar();
+
+/**
+ * Footer Component.
+ */
 get_footer();
