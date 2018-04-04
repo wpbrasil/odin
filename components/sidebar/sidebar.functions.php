fcf6b8b39f7eb88cd7c41f05e936b2f8cd2cb609
Adam Macias <eu@adammacias.com>
2018-04-04 12:27:26 -0300

    Improve widgets sidebar component

--- components/sidebar/sidebar.functions.php
+++ components/sidebar/sidebar.functions.php
@@ -12,16 +12,16 @@
  * @link https://developer.wordpress.org/reference/functions/register_sidebar/
  */
 
-if ( ! function_exists( 'odin_sidebar_widgets_init' ) ) {
+if ( ! function_exists( 'odin_widgets_sidebar_init' ) ) {
 
-	function odin_sidebar_widgets_init() {
+	function odin_widgets_sidebar_init() {
 
 		register_sidebar(
 			array(
-				'name' => __( 'Main Sidebar', 'odin' ),
-				'id' => 'sidebar-main',
-				'description' => __( 'Site Main Sidebar', 'odin' ),
-				'before_widget' => '<aside id="%1$s" class="odin-widget %2$s">',
+				'id' => 'odin-widgets-sidebar',
+				'name' => __( 'Sidebar', 'odin' ),
+				'description' => __( 'Widgets in Sidebar', 'odin' ),
+				'before_widget' => '<aside id="%1$s" class="odin-widget widget %2$s">',
 				'after_widget' => '</aside>',
 				'before_title' => '<h3 class="odin-widget-title">',
 				'after_title' => '</h3>',
@@ -30,6 +30,6 @@ if ( ! function_exists( 'odin_sidebar_widgets_init' ) ) {
 
 	}
 
-	add_action( 'widgets_init', 'odin_sidebar_widgets_init' );
+	add_action( 'widgets_init', 'odin_widgets_sidebar_init' );
 
 }
--- components/sidebar/sidebar.php
+++ components/sidebar/sidebar.php
@@ -14,13 +14,15 @@
 
 	<div class="odin-sidebar-wrapper">
 
-		<?php
-		if ( ! dynamic_sidebar( 'sidebar-main' ) ) {
-			the_widget( 'WP_Widget_Recent_Posts', array( 'number' => 10 ) );
-			the_widget( 'WP_Widget_Archives', array( 'count' => 0, 'dropdown' => 1 ) );
-			the_widget( 'WP_Widget_Tag_Cloud' );
-		}
-		?>
+		<div class="odin-sidebar__widgets">
+			<?php
+			if ( ! dynamic_sidebar( 'widgets-sidebar' ) ) {
+				the_widget( 'WP_Widget_Recent_Posts', array( 'number' => 10 ) );
+				the_widget( 'WP_Widget_Archives', array( 'count' => 0, 'dropdown' => 1 ) );
+				the_widget( 'WP_Widget_Tag_Cloud' );
+			}
+			?>
+		</div>
 
 	</div>
 
