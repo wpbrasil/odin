e2710312ac086a4b291f99b2accc5ecd909651d6
Adam Macias <eu@adammacias.com>
2018-04-04 10:34:27 -0300

    Update post component

--- components/post/post.php
+++ components/post/post.php
@@ -12,35 +12,24 @@
 
 	<div class="odin-post-wrapper">
 
-		<header class="odin-post-header">
-
-			<?php
-			if ( is_single() ) :
-				the_title( '<h1 class="odin-post-header__title">', '</h1>' );
-			else :
-				the_title( '<h2 class="odin-post-header__title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			endif;
-			?>
-
-			<div class="odin-post-header__meta">
-
-				<?php get_template_part( 'components/post/parts/meta-date-post' ); ?>
-
-				<?php get_template_part( 'components/post/parts/meta-author-post' ); ?>
-
-			</div>
-
-		</header>
+		<?php
+		/**
+		 * Header Post Component.
+		 */
+		get_template_part( 'components/post/parts/header-post' ); ?>
 
 		<div class="odin-post-body">
 
 			<?php
-			 if ( is_search() || is_archive() ) {
+			 if ( ! is_single() ) {
+
 			 	/**
 				 * Excerpt post.
 				 */
 			 	the_excerpt();
+
 			 } else {
+
 				/**
 				 * Content post.
 				 */
@@ -50,27 +39,16 @@
 				 * Pagination within a post.
 				 */
 				get_template_part( 'components/pagination/pagination', 'within-post' );
+
 			} ?>
 
 		</div>
 
-		<?php if ( is_single() ) : ?>
-			<footer class="odin-post-footer">
-
-				<div class="odin-post-footer__meta">
-
-					<?php get_template_part( 'components/post/parts/comments-link-post' ); ?>
-
-					<?php get_template_part( 'components/post/parts/taxonomy-category-post' ); ?>
-
-					<?php get_template_part( 'components/post/parts/taxonomy-tag-post' ); ?>
-
-					<?php get_template_part( 'components/pagination/pagination', 'between-single-posts' ); ?>
-
-				</div>
-
-			</footer>
-		<?php endif; ?>
+		<?php
+		/**
+		 * Footer Post Component.
+		 */
+		get_template_part( 'components/post/parts/footer-post' ); ?>
 
 	</div><!-- .odin-post-wrapper -->
 
